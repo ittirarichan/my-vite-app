2e39dfb343d4b5c8614ef7bea92fb00a7ad885f7
ittirarichan <abhishekbinish86@gmail.com>
2024-10-09 11:23:26 +0530

    react

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+
+//       <h1>Vite App</h1>
+//       <div className="card">
+//           <h2>count is {count}</h2>
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           +
+//         </button>
+//         <button onClick={() => count > 0 && setCount((count) => count - 1)}>
+//           -
+//         </button>
+//       </div>
+//     </>
+//   )
+// }
+
+function App(){
+  const data=[{
+    id:1,name:'akhil',age:20
+  },
+  {
+    id:2,name:'manu',age:21
+  },
+  {
+    id:3,name:'ammu',age:23
+  }]
+  return(
+    <>
+    <h1>student details</h1>
+    <table>
+      <thead>
+        <th>id</th>
+        <th>name</th>
+        <th>age</th>
+      </thead>
+      <tbody>
+        {data.map((element)=>(
+          <tr key={element.id}>
+            <td>{element.id}</td>
+            <td>{element.name}</td>
+            <td>{element.age}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </>
+  )
+}
+
+export default App
